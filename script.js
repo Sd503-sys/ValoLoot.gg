b862c5cddcb268eabf289a1e855c45271207cf26
Sd503-sys <jarolpicht@gmail.com>
2025-02-21 10:25:42 +0100

    add Weapon Generator

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+var weaponId;
 function warn() {
-   var randomNumber = Math.floor(Math.random() *6 +1);
-    alert('Forsaken Vandal ' + randomNumber);
+ //random Weapon generator
+ generateRandomWeapon();
+ //show random weapon
+ showRandomWeapon();
+}
+
+function generateRandomWeapon() {
+    var wId = Math.floor( Math.random()  *6 +1);
+    weaponId = wId;
+}
+
+function showRandomWeapon() {
+    console.log(weaponId);
 }
